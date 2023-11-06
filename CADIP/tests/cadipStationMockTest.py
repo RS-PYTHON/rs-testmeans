92c11d0cdc118fcfec634baf41fa3025f2172ed4
ovidiu <opadeanu@c-s.ro>
2023-11-06 12:18:29 +0200

    [RSPY39]: Tests: Updated tests to match new response body

--- CADIP/tests/cadipStationMockTest.py
+++ CADIP/tests/cadipStationMockTest.py
@@ -26,50 +26,47 @@ def testAuth(correct_login: str, incorrect_login: str):
 @pytest.mark.parametrize(
     "sessionResponse20230216",
     [
-        [
-            {
-                "AcquisitionId": "415_01",
-                "AntennaId": "SIV",
-                "AntennaStatusOK": True,
-                "DeliveryPushOK": True,
-                "DownlinkOrbit": "62343",
-                "DownlinkStart": "2023-05-01T12:15:34Z",
-                "DownlinkStatusOK": True,
-                "DownlinkStop": "2023-05-01T12:31:57Z",
-                "FrontEndId": "FEP_identifier",
-                "FrontEndStatusOK": True,
-                "Id": "another_session_id",
-                "NumChannels": "1",
-                "PlannedDataStart": "some_date",
-                "PlannedDataStop": "some_date",
-                "PublicationDate": "2023-02-16T12:00:00.000Z",
-                "Retransfer": False,
-                "Satellite": "S3A",
-                "SessionID": "S3OLCI1",
-                "StationUnitId": "00",
-            },
-        ],
+        {
+            "AcquisitionId": "415_01",
+            "AntennaId": "SIV",
+            "AntennaStatusOK": True,
+            "DeliveryPushOK": True,
+            "DownlinkOrbit": "62343",
+            "DownlinkStart": "2023-05-01T12:15:34Z",
+            "DownlinkStatusOK": True,
+            "DownlinkStop": "2023-05-01T12:31:57Z",
+            "FrontEndId": "FEP_identifier",
+            "FrontEndStatusOK": True,
+            "Id": "another_session_id",
+            "NumChannels": "1",
+            "PlannedDataStart": "some_date",
+            "PlannedDataStop": "some_date",
+            "PublicationDate": "2023-02-16T12:00:00.000Z",
+            "Retransfer": False,
+            "Satellite": "S3A",
+            "SessionID": "S3OLCI1",
+            "StationUnitId": "00",
+        },
     ],
 )
 def testQuerrySessions(sessionResponse20230216):
     # Response containing more than 1 result, since there are more products matching
     apiRoute = "Sessions?filter=PublicationDate gt 2019"
     data = requests.get(WEBSERVER + apiRoute)
-    assert len(json.loads(data.text)) > 1
+    assert len(data.text) > 1
     # Response containing exactly one item, since explicit date is mentioned.
     apiRoute = "Sessions?filter=PublicationDate eq 2023-02-16"
     data = requests.get(WEBSERVER + apiRoute)
-
-    assert len(json.loads(data.text)) == 1
+    assert isinstance(json.loads(data.text), dict)
     # Check response content with test-defined one.
     apiRoute = "Sessions?filter=PublicationDate eq 2023-02-16"
     data = requests.get(WEBSERVER + apiRoute)
+    assert json.loads(data.text).keys() == sessionResponse20230216.keys()
     assert json.loads(data.text) == sessionResponse20230216
-
     # Empty json response since there are no products older than 1999.
     apiRoute = "Sessions?filter=PublicationDate lt 1999"
     data = requests.get(WEBSERVER + apiRoute)
-    assert not len(json.loads(data.text))
+    assert not len(data.text)
 
 
 def testQuerryFiles():
