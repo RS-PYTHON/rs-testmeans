459595c63e86e97cb403490c720fa7540b9e8c0b
Ovidiu <opadeanu@c-s.ro>
2024-02-12 17:55:51 +0200

    Added tests to check attrs update

--- tests/test_dpr_processor_mock.py
+++ tests/test_dpr_processor_mock.py
@@ -1,3 +1,4 @@
+import json
 import pathlib
 
 import pytest
@@ -68,17 +69,17 @@ def test_list_of_downloadableable_products():
     assert dpr_processor.list_of_downloads
 
 
-@pytest.mark.unit()
+@pytest.mark.unit
 @pytest.mark.parametrize(
     "input_data_path, expected_new_product_name",
     [
         (
-            "tests/data/S1SEWRAW_20230103T225516_0038_A003_T290.zarr",
-            "tests/data/S1SEWRAW_20230103T225516_0038_A003_TEST_CRC.zarr",
+                "tests/data/S1SEWRAW_20230103T225516_0038_A003_T290.zarr",
+                "tests/data/S1SEWRAW_20230103T225516_0038_A003_TEST_CRC.zarr",
         ),
         (
-            "tests/data/S1SEWRAW_20230103T225516_0038_A003_T290.zarr.zip",
-            "tests/data/S1SEWRAW_20230103T225516_0038_A003_TEST_CRC.zarr.zip",
+                "tests/data/S1SEWRAW_20230103T225516_0038_A003_T290.zarr.zip",
+                "tests/data/S1SEWRAW_20230103T225516_0038_A003_TEST_CRC.zarr.zip",
         )
     ],
 )
@@ -89,3 +90,43 @@ def test_dpr_product_rename(mocker, input_data_path, expected_new_product_name):
     mock_rename.assert_called_once()
     # Check that pathlib.Path.rename is called with the right parameters
     mock_rename.assert_called_with(pathlib.PosixPath(input_data_path), expected_new_product_name)
+
+
+@pytest.mark.unit
+@pytest.mark.parametrize(
+    "input_data_path, expected_processing_stamp",
+    [
+        (
+                "tests/data/S1SEWRAW_20230103T225516_0038_A003_T290.zarr",
+                "RSPY_DprMockupProcessor",
+        )
+    ],
+)
+def test_dpr_attrs_update(mocker, input_data_path, expected_processing_stamp):
+    yamlstr = """
+    I/O:
+      inputs_products:
+      - id: CADU1
+        path: DCS_04_S1A_20231121072204051312_ch1_DSDB_00023.raw
+        store_type: zarr
+      output_products:
+      - id: outputs
+        path: src/DPR/data/ # output folder or S3 bucket
+    workflow:
+    - step: 1
+      parameters:
+        product_types:
+        - S1SEWRAW
+        - S1SIWRAW
+    """
+    initial_data = json.load(open(pathlib.Path(input_data_path) / ".zattrs"))
+    mocker.patch('src.DPR.DPRProcessor.DPRProcessor.update_product_name', return_value=None, autospec=True)
+
+    dpr = DPRProcessor(yaml.dump(yaml.safe_load(yamlstr)))
+    dpr.update_product(pathlib.Path(input_data_path))
+    updated_data = json.load(open(pathlib.Path(input_data_path) / ".zattrs"))
+    # Check that processing history timestamp was updated, and processor name is correct
+    assert initial_data != updated_data
+    assert expected_processing_stamp in updated_data["other_metadata"]["history"]['processor']
+    # Replace with initial data to avoid side effects
+    json.dump(initial_data, open(pathlib.Path(input_data_path) / ".zattrs", "w"), indent=4)
