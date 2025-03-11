fa56ba1cb00970fd9d82b959243a452d770e1484
Julien Gaucher <julien.gaucher@csgroup.eu>
2025-03-11 17:13:58 +0100

    test

--- testci.py
+++
@@ -1,9 +0,0 @@
-import requests
-import time
-
-for i in range(60):
-    try:
-        print(requests.get("http://prefect-server:4200/api/health"), flush=True)
-    except Exception:
-        print("KO", flush=True)
-    time.sleep(1)
