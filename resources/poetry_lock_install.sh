3d43f2bd6c1dc583ad6ba339a9d314ec0f5b1983
Julien Gaucher <139342493+jgaucher-cs@users.noreply.github.com>
2026-06-03 11:30:43 +0200

    Update poetry to 2.4.1 (#139)
    * Update poetry to 2.4.1
    
    * Fix safety

---
+++ resources/poetry_lock_install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# Copyright 2023-2026 CS Group
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -euo pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ROOT_DIR="$(realpath $SCRIPT_DIR/..)"
+
+# In each subproject, install dependencies with poetry
+for f in $(find "$ROOT_DIR" -name pyproject.toml); do
+    (set -x
+        cd $(dirname $f)
+        poetry lock --regenerate
+        poetry install --with dev || poetry install
+        if [[ " $@ " == *" --otel "* ]]; then
+            poetry run opentelemetry-bootstrap -a install || true # install otel instrumentation packages for dependencies
+        fi
+    )
+done
